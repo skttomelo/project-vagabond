2f07e881edae600a84ae1fe0bfba13fdd38a9b0d
Trevor <trevor.h.crow@gmail.com>
2020-08-20 17:11:37 -0400

    need to find struct used for ReadHalf from tokio

--- vagabond-server/src/entities.rs
+++ vagabond-server/src/entities.rs
@@ -1,5 +1,22 @@
+use std::str;
+
 use serde::{Serialize, Deserialize};
-use std::collections::HashMap;
+
+use tokio::io::{ReadHalf, AsyncRead, AsyncWrite};
+
+// use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Action {
+    Left,
+    Right,
+    Still,
+    Jumping,
+    Falling,
+    Moving,
+    Attacking,
+    Blocking,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Point2<T> {
@@ -17,15 +34,53 @@ pub struct Player {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entity {
     pub id: u32,
-    pub pos: Point2<f32>,
-    pub vel: Point2<f32>,
+    facing: Action, // Left or Right
+    movement: Action, // Still or Moving
+    stance: Action, // Attacking, Still, or Blocking
+    jumping: Action, // Jumping, Falling, or Still
+    pos: Point2<f32>,
+    vel: Point2<f32>,
     pub size: f32
 }
 
-// impl Entity {
-//     pub fn to_json(&self) -> String {
-//         format!("")
-//     }
-// }
+impl Entity {
+    pub fn update(&mut self) {
+        match self.movement {
+            Action::Moving => self.vel.x = 1.0,
+            Action::Still => self.vel.x = 0.0,
+            _ => ()
+        };
+
+        match self.facing {
+            Action::Left => {
+                if self.vel.x > 0.0 {
+                    self.vel.x = -self.vel.x;
+                }
+            },
+            Action::Right => {
+                if self.vel.x < 0.0 {
+                    self.vel.x = -self.vel.x
+                }
+            }
+        }
+
+
+
+        self.pos.x += self.vel.x;
+        self.pos.y += self.vel.y;
+    }
+
+    pub fn from_json(reader: &TcpStream) -> Entity {
+        let mut data = [0 as u8; 1024]; // byte buffer
+        let byte_data = reader.poll_read(&mut data);
+        let json = str::from_utf8(&byte_data).unwrap();
+
+        serde_json::from_str(json).unwrap()
+    }
+    
+    // pub fn to_json(&self) -> String {
+
+    // }
+}
 
 // pub type Entities = HashMap<u32,Entity>;
--- vagabond-server/src/main.rs
+++ vagabond-server/src/main.rs
@@ -1,67 +1,34 @@
-use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
-use std::io::{Read, Write};
-use serde_json;
-use serde::{Serialize, Deserialize};
+// use serde_json;
+// use serde::{Serialize, Deserialize};
+
+use tokio::net::{TcpListener, TcpStream};
+
+// use std::io;
 
 mod entities;
-use entities::Player;
+use entities::Entity;
 
-fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 1024]; // using 50 byte buffer
-    // let mut writer = BufWriter::new(&stream);
-    // let mut reader = BufReader::new(&stream);
-    // reader.read_exact((&mut response).unwrap();
-    // println!("{}",response);
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1::1337";
+    let mut listener = TcpListener::bind(addr).await.unwrap();
 
-    // let json = std::str::from_utf8(&data[..]).expect("could not read");
-    let mut deserialize_me = serde_json::Deserializer::from_reader(&stream);
-    let client: Player = Player::deserialize(&mut deserialize_me).unwrap();
-    // let client: Player = serde_json::from_str(&json).expect("could not deserialize data");
-    println!("{:?}", client);
-
-    while match stream.read(&mut data) {
-        Ok(_size) => {
-            // echo everything!
-            // let mut reader = BufReader::new(&stream);
-            // let mut response = String::from("");
-            // println!("{}", reader.read_to_string(&mut response));
-
-            // stream.write(&data[0..size]).unwrap();
-            true
-        },
-        Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {
+    loop {
+        let (mut socket, _) = listener.accept().await.unwrap();
         
-        break;
+        tokio::spawn(async move {
+            process_message(socket).await;
+        });
     }
-
-    stream.shutdown(Shutdown::Both).unwrap();
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port 1337");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
-        }
-    }
-    // close the socket server
-    drop(listener);
+async fn process_message(mut socket: TcpStream) {
+    let (mut reader, mut writer) = socket.split();
+
+    let ent: Entity = Entity::from_json(&reader); 
+
+    // player.update();
+
+    
+
 }
