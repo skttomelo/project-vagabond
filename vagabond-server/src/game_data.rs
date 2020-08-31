3fe3e3d54068592aec06414ca2b2c2d27d1c8a16
Trevor <trevor.h.crow@gmail.com>
2020-08-30 22:00:10 -0400

    removed pubs and added comments

--- vagabond-server/src/game_data.rs
+++ vagabond-server/src/game_data.rs
@@ -27,6 +27,8 @@ pub struct Point2<T> {
     pub y: T
 }
 
+
+// Deprecated
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Player {
     pos: Point2<f32>,
@@ -36,14 +38,14 @@ pub struct Player {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Entity {
-    pub id: u8,
+    id: u8,
     facing: Action, // Left or Right
     movement: Action, // Still or Moving
     stance: Action, // Attacking, Still, or Blocking
     jumping: Action, // Jumping, Falling, or Still
-    pub pos: Point2<f32>,
-    pub vel: Point2<f32>,
-    pub size: f32
+    pos: Point2<f32>,
+    vel: Point2<f32>,
+    size: f32
 }
 
 impl Entity {
